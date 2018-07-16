d33df0bf0df1d018eefea828d68554e5f4c08016
yancloud <820453496@qq.com>
2018-07-17 07:33:49 +0800

    master

---
+++ src/com/nujnay/djisktra/MyDjisktra.java
@@ -0,0 +1,12 @@
+package com.nujnay.djisktra;
+
+
+import java.util.HashMap;
+
+public class MyDjisktra {
+
+    public static void main(String[] strings) {
+        HashMap<Integer, HashMap<Integer, Integer>> hashMapHashMap = new HashMap<>();
+
+    }
+}
